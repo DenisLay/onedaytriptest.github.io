8d7d208b9e08dcc41509657ee3a93acc3ece8214
DenisLay <56198059+DenisLay@users.noreply.github.com>
2020-03-23 23:29:07 +0300

    Commit watchPosition
    Commit watchPosition

--- script.js
+++ script.js
@@ -7,19 +7,19 @@ window.onload = function (){
 function initMap(){
     
     pos = {lat: 50.449241, lng: 30.513511};
-    var options = {
+    var opt = {
         center: new google.maps.LatLng(pos.lat, pos.lng),
         zoom: 12
     }
     
-    map = new google.maps.Map(document.getElementById("map"), options);
+    map = new google.maps.Map(document.getElementById("map"), opt);
     
-    if (navigator.geolocation)
-        navigator.geolocation.getCurrentPosition(geoloactionSuccess, geolocationFailure, {enableHighAccuracy: true});
+    /*if (navigator.geolocation)
+        navigator.geolocation.getCurrentPosition(geoloactionSuccess, geolocationFailure, {enableHighAccuracy: true});*/
     
 }
 
-function geoloactionSuccess(position){
+/*function geoloactionSuccess(position){
     pos = {lat: position.coords.latitude, 
            lng: position.coords.longitude};
     
@@ -29,4 +29,28 @@ function geoloactionSuccess(position){
 
 function geolocationFailure(positionError){
     alert(positionError)
+}*/
+
+var id, options;
+
+function success(position) {
+  pos = {lat: position.coords.latitude, 
+           lng: position.coords.longitude};
+    
+    map.setCenter(pos);
+    marker = new google.maps.Marker({position: pos, map: map, title: "You are here"});
+    navigator.geolocation.clearWatch(id);
 }
+
+function error(err) {
+  console.warn('ERROR(' + err.code + '): ' + err.message);
+}
+
+
+options = {
+  enableHighAccuracy: true,
+  timeout: 5000,
+  maximumAge: 1000
+};
+
+id = navigator.geolocation.watchPosition(success, error, options);
