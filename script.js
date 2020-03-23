2a8a4758066d0a1f711af38742526fe298abaa81
DenisLay <56198059+DenisLay@users.noreply.github.com>
2020-03-23 23:04:44 +0300

    Commit enable High Accuracy
    enable High Accuracy

--- script.js
+++ script.js
@@ -1,36 +1,29 @@
+var map, pos, marker;
 
-
-alert("1");
 
 function initMap(){
     
-    var map, infoWindow, marker;
-    
-    alert("2");
-    
-    var pos = {lat: 50.449241, lng: 30.513511};
+    pos = {lat: 50.449241, lng: 30.513511};
     var options = {
         center: new google.maps.LatLng(pos.lat, pos.lng),
         zoom: 12
     }
     
-    var map = new google.maps.Map(document.getElementById("map"), options);
+    map = new google.maps.Map(document.getElementById("map"), options);
     
-    if (navigator.geolocation){
-        navigator.geolocation.getCurrentPosition(function (position){
-            pos = {
-                lat: position.coords.latitude,
-                lng: position.coords.longitude
-            }
-            
-            map.setCenter(pos);
-            
-            marker = new google.maps.Marker({position: pos, map: map, title: "You are here"});
-        })
-    }
-    else{
-        alert("Geolocation failed!");
-    }
+    if (navigator.geolocation)
+        navigator.geolocation.getCurrentPosition(geoloactionSuccess, geolocationFailure, {enableHighAccuracy: true});
     
+}
+
+function geoloactionSuccess(position){
+    pos = {lat: position.coords.latitude, 
+           lng: position.coords.longitude};
     
+    map.setCenter(pos);
+    marker = new google.maps.Marker({position: pos, map: map, title: "You are here"});
+}
+
+function geolocationFailure(positionError){
+    alert(positionError)
 }
