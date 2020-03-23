a23a9855aa3cf17dd7410d632d19358e26a9ea86
DenisLay <56198059+DenisLay@users.noreply.github.com>
2020-03-23 17:08:21 +0300

    Commit 4

--- script.js
+++ script.js
@@ -1,8 +1,10 @@
-var map, infoWindow, marker;
+
 
 alert("1");
 
-/*function initMap(){
+function initMap(){
+    
+    var map, infoWindow, marker;
     
     alert("2");
     
@@ -31,28 +33,4 @@ alert("1");
     }
     
     
-}*/
-
-var pos = {lat: 50.449241, lng: 30.513511};
-    var options = {
-        center: new google.maps.LatLng(pos.lat, pos.lng),
-        zoom: 12
-    }
-    
-    var map = new google.maps.Map(document.getElementById("map"), options);
-    
-    if (navigator.geolocation){
-        navigator.geolocation.getCurrentPosition(function (position){
-            pos = {
-                lat: position.coords.latitude,
-                lng: position.coords.longitude
-            }
-            
-            map.setCenter(pos);
-            
-            marker = new google.maps.Marker({position: pos, map: map, title: "You are here"});
-        })
-    }
-    else{
-        alert("Geolocation failed!");
-    }
+}
