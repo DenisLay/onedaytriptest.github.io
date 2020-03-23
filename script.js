122b8cfa272816b3078d261a46bf446237e0cbe6
DenisLay <56198059+DenisLay@users.noreply.github.com>
2020-03-23 17:05:15 +0300

    Commit 3

--- script.js
+++ script.js
@@ -2,7 +2,9 @@ var map, infoWindow, marker;
 
 alert("1");
 
-function initMap(){
+/*function initMap(){
+    
+    alert("2");
     
     var pos = {lat: 50.449241, lng: 30.513511};
     var options = {
@@ -20,10 +22,6 @@ function initMap(){
             }
             
             map.setCenter(pos);
-            /*infoWindow = new google.maps.InfoWindow;
-            infoWindow.setPosition(pos);
-            infoWindow.setContent("You are here");
-            infoWindow.open(map);*/
             
             marker = new google.maps.Marker({position: pos, map: map, title: "You are here"});
         })
@@ -33,4 +31,28 @@ function initMap(){
     }
     
     
-}
+}*/
+
+var pos = {lat: 50.449241, lng: 30.513511};
+    var options = {
+        center: new google.maps.LatLng(pos.lat, pos.lng),
+        zoom: 12
+    }
+    
+    var map = new google.maps.Map(document.getElementById("map"), options);
+    
+    if (navigator.geolocation){
+        navigator.geolocation.getCurrentPosition(function (position){
+            pos = {
+                lat: position.coords.latitude,
+                lng: position.coords.longitude
+            }
+            
+            map.setCenter(pos);
+            
+            marker = new google.maps.Marker({position: pos, map: map, title: "You are here"});
+        })
+    }
+    else{
+        alert("Geolocation failed!");
+    }
